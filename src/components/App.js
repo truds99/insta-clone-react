4337246230240adbda6093a5fa19432fa7b2a71e
truds99 <dudscardoso9@gmail.com>
2024-02-04 18:15:27 -0300

    basic components

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import Navbar from './Navbar';
+import Corpo from './Corpo';
+import FundoMobile from './FundoMobile'
+
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Corpo />
+      <FundoMobile />
+    </div>
+  )
+}
+
+export default App;
