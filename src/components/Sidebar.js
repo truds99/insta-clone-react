f7b5370a4ebf80bdf5f3899caf136a84669d605b
truds99 <dudscardoso9@gmail.com>
2024-02-04 22:46:03 -0300

    ready components

---
+++ src/components/Sidebar.js
@@ -0,0 +1,84 @@
+function Usuario(props) {
+    return (
+        <div class="usuario">
+            <img src={props.img} />
+            <div class="texto">
+                <strong>catanacomics</strong>
+                <span>
+                    {props.user}
+                    <ion-icon name="pencil"></ion-icon>
+                </span>
+            </div>
+        </div>
+    )
+}
+
+function Sugestao(props) {
+    return (
+        <div class="sugestao">
+            <div class="usuario">
+                <img src={props.img} />
+                <div class="texto">
+                    <div class="nome">{props.nome}</div>
+                    <div class="razao">{props.razao}</div>
+                </div>
+            </div>
+
+            <div class="seguir">Seguir</div>
+        </div>
+    )
+}
+
+const sugestaoData = [
+    {
+        img: "assets/img/bad.vibes.memes.svg",
+        nome: "bad.vibes.memes",
+        razao: "Segue você",
+    },
+    {
+        img: "assets/img/chibirdart.svg",
+        nome: "chibirdart",
+        razao: "Segue você",
+    },
+    {
+        img: "assets/img/razoesparaacreditar.svg",
+        nome: "razoesparaacreditar",
+        razao: "Novo no Instagram",
+    },
+    {
+        img: "assets/img/adorable_animals.svg",
+        nome: "adorable_animals",
+        razao: "Segue você",
+    },
+    {
+        img: "assets/img/smallcutecats.svg",
+        nome: "smallcutecats",
+        razao: "Segue você",
+    },
+];
+
+
+export default function Sidebar() {
+    return (
+        <div class="sidebar">
+            <Usuario user='Catana' img='assets/img/catanacomics.svg'/>
+
+            <div class="sugestoes">
+                <div class="titulo">
+                    Sugestões para você
+                    <div>Ver tudo</div>
+                </div>
+                {sugestaoData.map(elm => <Sugestao img={elm.img} nome={elm.nome} razao={elm.razao}/>)}
+            </div>
+
+            <div class="links">
+                Sobre • Ajuda • Imprensa • API • Carreiras • Privacidade • Termos • Localizações • Contas mais relevantes •
+                Hashtags • Idioma
+            </div>
+
+            <div class="copyright">
+                © 2021 INSTAGRAM DO FACEBOOK
+            </div>
+        </div>
+    )
+}
